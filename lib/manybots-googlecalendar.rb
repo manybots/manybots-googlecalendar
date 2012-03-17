138f4c85bb7b87368af1dde9f12442d37ba408ec
Alexandre Loureiro Solleiro <alex@webcracy.org>
2012-03-17 12:11:40 +0000

    correct class variables settings

--- lib/manybots-googlecalendar.rb
+++ lib/manybots-googlecalendar.rb
@@ -3,11 +3,11 @@ require "manybots-googlecalendar/engine"
 module ManybotsGooglecalendar
   # Google App Id for OAuth2
   mattr_accessor :google_app_id
-  @@github_app_id = nil
+  @@google_app_id = nil
 
   # Google App Secret for OAuth2
   mattr_accessor :google_app_secret
-  @@github_app_secret = nil
+  @@google_app_secret = nil
   
   mattr_accessor :app
   @@app = nil
